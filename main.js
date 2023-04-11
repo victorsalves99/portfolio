0e5555907c4e974858b0a99d60ee80a5e15f5a6d
victorsalves99 <victorsalves99@outlook.com>
2023-04-11 19:06:28 -0300

    criada a pagina home

---
+++ main.js
@@ -0,0 +1,7 @@
+
+// Responsividade do menu Hamburger
+document.querySelector(".menu-active").addEventListener("click",() => {
+
+    const navigation = document.querySelector(".navbar-navigation")
+     navigation.classList.toggle("response-active")
+})
