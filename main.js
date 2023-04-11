e9b9084b5a0fa109cbdbee579da43781f158b30a
victorsalves99 <victorsalves99@outlook.com>
2023-04-11 20:07:37 -0300

    Trocado de posição o texto e a imagem e responsividade aplicada.

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
-
 // Responsividade do menu Hamburger
-document.querySelector(".menu-active").addEventListener("click",() => {
-
-    const navigation = document.querySelector(".navbar-navigation")
-     navigation.classList.toggle("response-active")
-})
+document.querySelector(".menu-active").addEventListener("click", () => {
+  const navigation = document.querySelector(".navbar-navigation");
+  navigation.classList.toggle("response-active");
+});
